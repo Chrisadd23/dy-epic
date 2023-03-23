f8cc4b82fbd5b448268caa856caa4978c009d055
Chrisadd23 <haas.christophermichael@gmx.de>
2023-03-23 22:50:36 +0100

    create: login
      - integrate clean architecture

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 import 'package:amplify_flutter/amplify_flutter.dart';
-import 'package:app_flutter_produkt_bestellen/amplifyconfiguration.dart';
+import 'package:app_flutter_produkt_bestellen/core/amplify/amplifyconfiguration.dart';
 import 'package:flutter/material.dart';
 
 Future<void> main() async {
