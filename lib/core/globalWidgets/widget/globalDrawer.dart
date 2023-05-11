d01523619e620528764dfd64af675013596496ca
Chrisadd23 <haas.christophermichael@gmx.de>
2023-05-11 22:11:46 +0200

    create: Drawer integration

--- lib/core/globalWidgets/widget/globalDrawer.dart
+++ lib/core/globalWidgets/widget/globalDrawer.dart
@@ -1,10 +1,42 @@
 import 'package:flutter/material.dart';
 
+import '../../../gen/assets.gen.dart';
+
 class GlobalDrawer extends StatelessWidget {
   const GlobalDrawer({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Drawer();
+    return SizedBox(
+    width: MediaQuery.of(context).size.width * 0.702,
+        child:  Drawer(
+          child: Column(
+            children: [
+              Container(
+                decoration: const BoxDecoration(
+          border: Border(
+          top: BorderSide(color: Colors.black, width: 2),
+        )),
+                child: Container(
+                  height: 96,
+                  padding: const EdgeInsets.only(top: 28,bottom: 20, right: 5,left: 4),
+                  width: MediaQuery.of(context).size.width * 0.7,
+                  decoration:  const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(bottomRight: Radius.circular(20),
+                      ),
+                      boxShadow: [
+                        BoxShadow(color: Colors.black,blurStyle: BlurStyle.inner,offset: Offset(0,2)),
+                      ]
+                  ),
+                  child: Image.asset(
+                    Assets.company.appBarLogo.path,
+                    fit: BoxFit.fitWidth,
+                  ),
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
