678ae20c84b903558d144fc3d038354287d417d4
Christopher Haas <christopherhaas@MBPvonCistopher.fritz.box>
2023-05-26 10:28:59 +0200

    Drawer integrate icon

--- lib/core/global_widgets/widget/globalDrawer.dart
+++ lib/core/global_widgets/widget/globalDrawer.dart
@@ -1,6 +1,6 @@
 import 'package:app_flutter_produkt_bestellen/gen/assets.gen.dart';
 import 'package:flutter/material.dart';
-
+import 'package:icon_decoration/icon_decoration.dart';
 
 class GlobalDrawer extends StatelessWidget {
   const GlobalDrawer({Key? key}) : super(key: key);
@@ -14,52 +14,114 @@ class GlobalDrawer extends StatelessWidget {
             borderRadius: BorderRadius.only(bottomRight: Radius.circular(20))
           ),*/
           child: Container(
-            decoration:  BoxDecoration(
+            decoration: BoxDecoration(
                 color: const Color.fromRGBO(87, 87, 87, 0.0),
-                border: Border.all(color: Colors.black,width: 2)),
+                border: Border.all(color: Colors.black, width: 2)),
             child: Column(
               children: [
                 Container(
                   decoration: const BoxDecoration(
-                    borderRadius: BorderRadius.only(bottomRight: Radius.circular(20),bottomLeft: Radius.circular(20))
-                        ,color: Color.fromRGBO(221, 221, 221, 1),
+                      borderRadius: BorderRadius.only(
+                          bottomRight: Radius.circular(20),
+                          bottomLeft: Radius.circular(20)),
+                      color: Color.fromRGBO(221, 221, 221, 1),
                       boxShadow: [
                         BoxShadow(
                             color: Colors.black,
                             blurStyle: BlurStyle.inner,
                             offset: Offset(0, 2)),
-                      ]
-                  ),
-                  child: Column(children: [
-                    Container(
-                      height: 97,
-                      width: double.infinity,
-                      decoration: const BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.only(
-                            bottomRight: Radius.circular(20),
+                      ]),
+                  child: Column(
+                    children: [
+                      Container(
+                        height: 97,
+                        width: double.infinity,
+                        decoration: const BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.only(
+                              bottomRight: Radius.circular(20),
+                            ),
+                            boxShadow: [
+                              BoxShadow(
+                                  color: Colors.black,
+                                  blurStyle: BlurStyle.inner,
+                                  offset: Offset(0, 2)),
+                            ]),
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 5, right: 5),
+                          child: Image.asset(
+                            Assets.company.appBarLogo.path,
+                            fit: BoxFit.fitWidth,
                           ),
-                          boxShadow: [
-                            BoxShadow(
-                                color: Colors.black,
-                                blurStyle: BlurStyle.inner,
-                                offset: Offset(0, 2)),
-                          ]),
-                      child: Padding(
-                        padding: const EdgeInsets.only(left: 5,right: 5),
-                        child: Image.asset(
-                          Assets.company.appBarLogo.path,
-                          fit: BoxFit.fitWidth,
                         ),
                       ),
-                    ),
-                    const SizedBox(
-                      width: double.infinity,
-                      height: 90,
-                      child: Center(child: Text('hallo')),
+                      SizedBox(
+                        width: double.infinity,
+                        height: 90,
+                        child: Row(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(left: 8),
+                              child: Container(
+                                height: 75,
+                                width: 75,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(100),
+                                  color:
+                                  const Color.fromRGBO(247, 165, 64, 1.0),
+                                  boxShadow: const [
+                                    BoxShadow(
+                                        color: Colors.black,
+                                        blurStyle: BlurStyle.inner,
+                                        offset: Offset(0, 1)),
+                                    BoxShadow(
+                                        color: Colors.black,
+                                        blurStyle: BlurStyle.inner,
+                                        offset: Offset(0, -1)),
+                                    BoxShadow(
+                                        color: Colors.black,
+                                        blurStyle: BlurStyle.inner,
+                                        offset: Offset(1, 0)),
+                                    BoxShadow(
+                                        color: Colors.black,
+                                        blurStyle: BlurStyle.inner,
+                                        offset: Offset(-1, 0)),
+                                  ]
+                                ),
 
-                    )
-                  ],),
+                                child: const Padding(
+                                  padding: EdgeInsets.only(bottom: 5),
+                                  child: Icon(
+                                      Icons.person,
+                                      color: Color.fromRGBO(221, 221, 221, 1),
+                                      size: 70,
+                                      shadows: [
+                                        BoxShadow(
+                                            color: Colors.black,
+                                            blurStyle: BlurStyle.inner,
+                                            offset: Offset(0, 1)),
+                                        BoxShadow(
+                                            color: Colors.black,
+                                            blurStyle: BlurStyle.inner,
+                                            offset: Offset(0, -1)),
+                                        BoxShadow(
+                                            color: Colors.black,
+                                            blurStyle: BlurStyle.inner,
+                                            offset: Offset(1, 0)),
+                                        BoxShadow(
+                                            color: Colors.black,
+                                            blurStyle: BlurStyle.inner,
+                                            offset: Offset(-1, 0)),
+                                      ],
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
                 )
               ],
             ),
