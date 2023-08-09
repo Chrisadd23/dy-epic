11e3f8237177b2e2c56cf69e8ec3a61b1d129ea6
Christopher Haas <christopherhaas@MacBook-Pro-von-Christopher.local>
2023-08-09 19:41:27 +0200

    feat (AOPB-2):
      - create Entity at the current state
      - create abstract model for the categories

---
+++ lib/features/category/share/data/model/model_category_abstract.dart
@@ -0,0 +1,5 @@
+import 'package:app_flutter_produkt_bestellen/features/category/share/domain/entity/entity_category.dart';
+
+abstract class ModelCategoryAbstract {
+  EntityCategory toEntity();
+}
---
+++ lib/features/category/share/domain/entity/entity_category.dart
@@ -0,0 +1,8 @@
+import '../../presentation/cubit/state_category_generic.dart';
+
+class EntityCategory {
+  const EntityCategory({
+    required String categoryName,
+    required ProductCategory productCategory,
+  });
+}
