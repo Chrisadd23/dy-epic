3057b91085606e5f345d68886f5606632b7bf6ae
Christopher Haas <christopherhaas@MacBook-Pro-von-Christopher.fritz.box>
2023-10-19 20:50:00 +0200

    fix: add debug method

--- lib/features/products/presentation/page/product_page.dart
+++ lib/features/products/presentation/page/product_page.dart
@@ -1,5 +1,6 @@
 import 'package:app_flutter_produkt_bestellen/core/global_widgets/page/globa_page_widget.dart';
 import 'package:app_flutter_produkt_bestellen/features/products/presentation/widget/product_widget.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
@@ -12,5 +13,11 @@ class ProductPage extends HookWidget {
   Widget build(BuildContext context) {
     return GlobalScaffold(
         appBarContext: context, body: ProductWidget(product: product));
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties.add(StringProperty('product', product));
   }
 }
