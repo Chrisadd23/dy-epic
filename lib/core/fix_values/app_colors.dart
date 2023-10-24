4bcf916258f96c88d5678ce735aff864036ca5d9
Christopher Haas <christopherhaas@MBPvonCistopher.fritz.box>
2023-10-24 22:13:41 +0200

    fix:
      - use Stack for more flexibility

--- lib/core/fix_values/app_colors.dart
+++ lib/core/fix_values/app_colors.dart
@@ -6,6 +6,8 @@ class AppColors {
   static const Color black080808 = Color(0xFF080808);
 
   static const Color grey8D8D8E = Color(0xFF8D8D8E);
+  static const Color greyD7D7D7 = Color(0xFFD7D7D7);
+  static const Color greyA7A7A7 = Color(0xFFA7A7A7);
 
   static const Color whiteD6D6D7 = Color(0xFFD6D6D7);
 }
--- lib/features/products/presentation/widget/product_widget.dart
+++ lib/features/products/presentation/widget/product_widget.dart
@@ -14,43 +14,52 @@ class ProductWidget extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(builder: (context, constraints) {
-      return SizedBox(
-        width: double.infinity,
-        child: Column(
-          children: <Widget>[
-            SizedBox(
-              height: constraints.maxHeight * 0.03,
-            ),
-            Container(
-              height: constraints.maxHeight * 0.07,
-              width: constraints.maxWidth * 0.5,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(40),
-                  color: AppColors.grey8D8D8E.withOpacity(0.4)),
-              child: const Center(
-                child: Text(
-                  'Produkt',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 35,
-                      fontWeight: FontWeight.bold,
-                      shadows: [
-                        Shadow(color: Colors.black, offset: Offset(1, 1)),
-                        Shadow(color: Colors.black, offset: Offset(-1, 1)),
-                        Shadow(color: Colors.black, offset: Offset(-1, -1)),
-                        Shadow(color: Colors.black, offset: Offset(1, -1)),
-                      ]),
+    return SizedBox(
+      width: double.infinity,
+      height: MediaQuery.of(context).size.height,
+      child: Stack(
+        children: <Widget>[
+          Align(
+            alignment: Alignment.topCenter,
+            child: Padding(
+              padding: EdgeInsets.only(
+                  top: MediaQuery.of(context).size.height * 0.015),
+              child: Container(
+                height: MediaQuery.sizeOf(context).height * 0.05,
+                width: MediaQuery.sizeOf(context).width * 0.5,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(40),
+                    color: AppColors.grey8D8D8E.withOpacity(0.4)),
+                child: const Center(
+                  child: Text(
+                    'Produkt',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 35,
+                        fontWeight: FontWeight.bold,
+                        shadows: [
+                          Shadow(color: Colors.black, offset: Offset(1, 1)),
+                          Shadow(color: Colors.black, offset: Offset(-1, 1)),
+                          Shadow(color: Colors.black, offset: Offset(-1, -1)),
+                          Shadow(color: Colors.black, offset: Offset(1, -1)),
+                        ]),
+                  ),
                 ),
               ),
             ),
-            SizedBox(
-              height: constraints.maxHeight * 0.03,
-            ),
-            Flexible(
+          ),
+          // ),
+          // SizedBox(
+          //   height: MediaQuery.sizeOf(context).height * 0.02,
+          // ),
+          Align(
+            alignment: Alignment.topCenter,
+            child: Padding(
+              padding: EdgeInsets.only(
+                  top: MediaQuery.of(context).size.height * 0.08),
               child: Container(
-                width: constraints.maxWidth * 0.9,
-                height: constraints.maxHeight * 0.3,
+                width: MediaQuery.sizeOf(context).width * 0.9,
+                height: MediaQuery.sizeOf(context).height * 0.3,
                 decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(20),
                     border: Border.all(color: Colors.black),
@@ -68,18 +77,62 @@ class ProductWidget extends HookWidget {
                       ),
               ),
             ),
-            SizedBox(
-              height: constraints.maxHeight * 0.05,
+          ),
+          // SizedBox(
+          //   height: MediaQuery.sizeOf(context).height * 0.02,
+          // ),
+          Align(
+              alignment: Alignment.center,
+              child: Padding(
+                padding: EdgeInsets.only(
+                    top: MediaQuery.of(context).size.height * 0.1),
+                child: const ProductCountWidget(),
+              )),
+          // SizedBox(
+          //   height: MediaQuery.sizeOf(context).height * 0.02,
+          // ),
+          Align(
+            alignment: Alignment.center,
+            child: Padding(
+              padding: EdgeInsets.only(
+                  top: MediaQuery.sizeOf(context).height * 0.48),
+              child: Container(
+                height: MediaQuery.sizeOf(context).height * 0.22,
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black),
+                    borderRadius: BorderRadius.circular(20)),
+                child: SingleChildScrollView(
+                  child: SizedBox(
+                    height: MediaQuery.sizeOf(context).height * 0.5,
+                    width: MediaQuery.sizeOf(context).width * 0.8,
+                    child: LayoutBuilder(builder: (context, scrollConstraints) {
+                      return Column(
+                        children: [
+                          ProductColorWidget(
+                              scrollConstraints: scrollConstraints),
+                          ProductColorWidget(
+                              scrollConstraints: scrollConstraints),
+                          ProductColorWidget(
+                              scrollConstraints: scrollConstraints),
+                          ProductColorWidget(
+                              scrollConstraints: scrollConstraints),
+                        ],
+                      );
+                    }),
+                  ),
+                ),
+              ),
             ),
-            const Flexible(child: ProductCountWidget()),
-            SizedBox(
-              height: constraints.maxHeight * 0.05,
-            ),
-            const ProductColorWidget(),
-          ],
-        ),
-      );
-    });
+          ),
+          // Flexible(
+          //     child: Container(
+          //   height: MediaQuery.of(context).size.height * 0.1,
+          //   width: MediaQuery.of(context).size.width * 0.9,
+          //   color: Colors.green,
+          // ))
+        ],
+      ),
+    );
   }
 
   @override
@@ -91,16 +144,20 @@ class ProductWidget extends HookWidget {
 
 class ProductColorWidget extends StatelessWidget {
   const ProductColorWidget({
+    required this.scrollConstraints,
     super.key,
   });
+
+  final BoxConstraints scrollConstraints;
 
   @override
   Widget build(BuildContext context) {
     return Flexible(
-      child: LayoutBuilder(builder: (context, constraints) {
-        return Container(
-          width: constraints.maxWidth * 0.9,
-          height: constraints.maxHeight * 0.6,
+      child: Padding(
+        padding: const EdgeInsets.only(bottom: 15),
+        child: Container(
+          width: scrollConstraints.maxWidth,
+          height: scrollConstraints.maxHeight * 0.25,
           decoration: BoxDecoration(
               color: AppColors.grey8D8D8E.withOpacity(0.4),
               borderRadius: BorderRadius.circular(20)),
@@ -110,25 +167,36 @@ class ProductColorWidget extends StatelessWidget {
               children: [
                 Flexible(
                   child: Container(
-                    height: currentConstraints.maxHeight * 0.4,
+                    height: currentConstraints.maxHeight * 0.5,
                     width: currentConstraints.maxWidth * 0.4,
                     decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(10),
                         border: Border.all(width: 2),
                         color: AppColors.grey8D8D8E.withOpacity(0.4)),
                     child: const Center(
+                        child: Padding(
+                      padding: EdgeInsets.all(5.0),
+                      child: FittedBox(
+                        fit: BoxFit.fill,
                         child: Text(
-                      'Farben',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 25,
-                          fontWeight: FontWeight.bold,
-                          shadows: [
-                            Shadow(color: Colors.black, offset: Offset(1, 1)),
-                            Shadow(color: Colors.black, offset: Offset(-1, 1)),
-                            Shadow(color: Colors.black, offset: Offset(-1, -1)),
-                            Shadow(color: Colors.black, offset: Offset(1, -1)),
-                          ]),
+                          'Gestellfarbe',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold,
+                              shadows: [
+                                Shadow(
+                                    color: Colors.black, offset: Offset(1, 1)),
+                                Shadow(
+                                    color: Colors.black, offset: Offset(-1, 1)),
+                                Shadow(
+                                    color: Colors.black,
+                                    offset: Offset(-1, -1)),
+                                Shadow(
+                                    color: Colors.black, offset: Offset(1, -1)),
+                              ]),
+                        ),
+                      ),
                     )),
                   ),
                 ),
@@ -145,9 +213,16 @@ class ProductColorWidget extends StatelessWidget {
               ],
             );
           }),
-        );
-      }),
+        ),
+      ),
     );
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties.add(DiagnosticsProperty<BoxConstraints>(
+        'scrollConstraints', scrollConstraints));
   }
 }
 
@@ -159,65 +234,61 @@ class ProductCountWidget extends HookWidget {
     return LayoutBuilder(builder: (context, constraints) {
       return Container(
           width: constraints.maxWidth * 0.8,
-          height: constraints.maxHeight * 0.4,
+          height: constraints.maxHeight * 0.15,
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(20),
-            color: const Color.fromRGBO(87, 87, 87, 0.1),
+            color: AppColors.greyD7D7D7,
           ),
           child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: <Widget>[
-                Flexible(
-                  child: InkWell(
-                    child: Container(
-                      width: constraints.maxWidth * 0.15,
-                      height: constraints.maxHeight * 0.3,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: const Color.fromRGBO(87, 87, 87, 0.4),
-                        image: DecorationImage(
-                          image: AssetImage(
-                              Assets.appComponents.png.vectorMinus.path),
-                        ),
+                InkWell(
+                  child: Container(
+                    width: constraints.maxWidth * 0.15,
+                    height: constraints.maxHeight * 0.1,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: AppColors.greyA7A7A7,
+                      image: DecorationImage(
+                        image: AssetImage(
+                            Assets.appComponents.png.vectorMinus.path),
                       ),
                     ),
-                    onTapDown: (details) {
-                      context.read<CubitProduct>().decrement();
-                    },
-                    onTapUp: (details) =>
-                        context.read<CubitProduct>().stopCounting(),
-                    onTapCancel: () =>
-                        context.read<CubitProduct>().stopCounting(),
                   ),
+                  onTapDown: (details) {
+                    context.read<CubitProduct>().decrement();
+                  },
+                  onTapUp: (details) =>
+                      context.read<CubitProduct>().stopCounting(),
+                  onTapCancel: () =>
+                      context.read<CubitProduct>().stopCounting(),
                 ),
-                Flexible(
-                  child: InkWell(
-                    child: Container(
-                      width: constraints.maxWidth * 0.15,
-                      height: constraints.maxHeight * 0.3,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: const Color.fromRGBO(87, 87, 87, 0.4),
-                        image: DecorationImage(
-                          image: AssetImage(
-                              Assets.appComponents.png.vectorPlus.path),
-                        ),
+                InkWell(
+                  child: Container(
+                    width: constraints.maxWidth * 0.15,
+                    height: constraints.maxHeight * 0.1,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: const Color.fromRGBO(87, 87, 87, 0.4),
+                      image: DecorationImage(
+                        image: AssetImage(
+                            Assets.appComponents.png.vectorPlus.path),
                       ),
                     ),
-                    onTapDown: (details) {
-                      context.read<CubitProduct>().increment();
-                    },
-                    onTapUp: (details) =>
-                        context.read<CubitProduct>().stopCounting(),
-                    onTapCancel: () =>
-                        context.read<CubitProduct>().stopCounting(),
                   ),
+                  onTapDown: (details) {
+                    context.read<CubitProduct>().increment();
+                  },
+                  onTapUp: (details) =>
+                      context.read<CubitProduct>().stopCounting(),
+                  onTapCancel: () =>
+                      context.read<CubitProduct>().stopCounting(),
                 ),
                 BlocBuilder<CubitProduct, StateProduct>(
                     builder: (context, state) {
                   return Container(
                     width: constraints.maxWidth * 0.35,
-                    height: constraints.maxHeight * 0.25,
+                    height: constraints.maxHeight * 0.08,
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(10),
                       color: const Color.fromRGBO(87, 87, 87, 0.4),
