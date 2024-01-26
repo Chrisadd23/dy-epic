863639a80a1a3d741ef9391f867673080a3e5f14
Christopher Haas <christopherhaas@MBPvonCistopher.fritz.box>
2024-01-26 14:27:53 +0100

    refactor: linter rules
      - delete unused import

--- lib/features/product/share/presentation/cubit/state_product.dart
+++ lib/features/product/share/presentation/cubit/state_product.dart
@@ -1,5 +1,4 @@
 import 'package:app_flutter_produkt_bestellen/features/product/share/domain/entity/entity_product.dart';
-import 'package:app_flutter_produkt_bestellen/core/error/failure_state.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'state_product.freezed.dart';
