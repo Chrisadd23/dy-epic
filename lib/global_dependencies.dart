220d87dee545a032bb6bfe4639eabe5ba0c0c7c6
Christopher Haas <christopherhaas@MBPvonCistopher.fritz.box>
2024-01-26 14:28:54 +0100

    refactor: linter rules

--- lib/global_dependencies.dart
+++ lib/global_dependencies.dart
@@ -1,11 +1,10 @@
 import 'package:app_flutter_produkt_bestellen/core/dependencies.dart';
 import 'package:app_flutter_produkt_bestellen/features/category/dependencies.dart';
 import 'package:app_flutter_produkt_bestellen/features/home/dependencies.dart';
+import 'package:app_flutter_produkt_bestellen/features/login/dependencies.dart';
 import 'package:app_flutter_produkt_bestellen/features/product/dependencies.dart';
 import 'package:app_flutter_produkt_bestellen/features/shopping_basket/dependencies.dart';
 import 'package:get_it/get_it.dart';
-
-import 'features/login/dependencies.dart';
 
 final getIt = GetIt.instance;
 
