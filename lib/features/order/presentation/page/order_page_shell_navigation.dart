ac5986bcf96158156445911b66043f443ae687de
Christopher Haas <christopherhaas@MBPvonCistopher.fritz.box>
2024-02-03 23:17:44 +0100

    feat:
       - change svg image color
       - change svg size

--- lib/features/order/presentation/page/order_page_shell_navigation.dart
+++ lib/features/order/presentation/page/order_page_shell_navigation.dart
@@ -29,8 +29,8 @@ class OrderPageShellNavigation extends StatelessWidget {
         items: [
           SvgPicture.asset(
             Assets.appComponents.svg.orderBasket,
-            width: 45,
-            height: 45,
+            width: 50,
+            height: 50,
           ),
           SvgPicture.asset(
             Assets.appComponents.svg.orderRequest,
