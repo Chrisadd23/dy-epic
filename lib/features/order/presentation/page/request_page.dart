084274d368d334514d1bb52f7e65ef33e467293d
Christopher Haas <christopherhaas@MBPvonCistopher.fritz.box>
2024-02-04 09:54:20 +0100

    feat:
       - integrated order titles

--- lib/features/order/presentation/page/request_page.dart
+++ lib/features/order/presentation/page/request_page.dart
@@ -1,3 +1,5 @@
+import 'package:app_flutter_produkt_bestellen/core/fix_values/app_colors.dart';
+import 'package:app_flutter_produkt_bestellen/core/fix_values/app_text_style.dart';
 import 'package:app_flutter_produkt_bestellen/features/shopping_basket/presentation/widget/shopping_basket_dialog.dart';
 import 'package:flutter/material.dart';
 
@@ -6,12 +8,28 @@ class RequestPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Stack(
+    return Stack(
       children: [
-        Center(
-          child: Text("anfrage"),
+        Align(
+          alignment: Alignment.topCenter,
+          child: Padding(
+            padding: const EdgeInsets.only(top: 20.0),
+            child: Container(
+              height: MediaQuery.sizeOf(context).height * 0.04,
+              width: MediaQuery.sizeOf(context).width * 0.5,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: AppColors.orangeF6A440),
+              child: FittedBox(
+                child: Text(
+                  "Anfragen",
+                  style: AppTextStyle.colorWhiteSize20ShadowBlack,
+                ),
+              ),
+            ),
+          ),
         ),
-        DialogShoppingBasket()
+        const DialogShoppingBasket()
       ],
     );
   }
