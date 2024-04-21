1a8b691e071fb74e25439e841da15dece3c074b1
Christopher Haas <christopherhaas@MBPvonCistopher.fritz.box>
2024-04-21 20:17:52 +0200

    refactor: linter rules

--- test/mock_data.dart
+++ test/mock_data.dart
@@ -1,5 +1,5 @@
 //MockData wich is use for Unit-Tests
 import 'package:app_flutter_produkt_bestellen/features/product/arbeitstische/domain/entity/entity_product_workingtable.dart';
 
-final mockEntityWorkingTableProduct =
+const mockEntityWorkingTableProduct =
     EntityWorkingTableProduct(name: '', productNumber: '', attributes: []);
