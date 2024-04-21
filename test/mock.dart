8e07f0ce189c578f1f8f2e42625100c3e8995b38
Christopher Haas <christopherhaas@MBPvonCistopher.fritz.box>
2024-04-21 20:16:35 +0200

    chore:
      - add unit-test for pipline test
      - test has no usage yet

---
+++ test/mock.dart
@@ -0,0 +1,6 @@
+//Product ----
+import 'package:app_flutter_produkt_bestellen/features/product/arbeitstische/data/datasource/datasource_product_arbeitstische.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockDataSourceProductWorkingTable extends Mock
+    implements DataSourceProductWorkingTable {}
---
+++ test/product/data/datasource/conference_chair/product_remote_data_source.dart
@@ -0,0 +1,29 @@
+import 'package:either_dart/either.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+import '../../../../mock.dart';
+import '../../../../mock_data.dart';
+
+void main() {
+  late final MockDataSourceProductWorkingTable
+      mockDataSourceProductWorkingTable;
+  group('test product datasource handling conference chair', () {
+    setUp(() {
+      mockDataSourceProductWorkingTable = MockDataSourceProductWorkingTable();
+    });
+
+    //unnecessary test only for ci handling will be changed if refactor-9 is ready
+    test(
+      'Get Product Data conference chaire',
+      () async {
+        when(() => mockDataSourceProductWorkingTable.loadData(any()))
+            .thenAnswer((_) async => Right(mockEntityWorkingTableProduct));
+
+        final dummyData =
+            await mockDataSourceProductWorkingTable.loadData('productNumber');
+        expect(dummyData, dummyData);
+      },
+    );
+  });
+}
