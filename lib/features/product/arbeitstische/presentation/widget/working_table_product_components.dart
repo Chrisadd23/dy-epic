8c4031b0da5cbbfc12860cc7c6a90370b3ced427
Christopher Haas <christopherhaas@MBPvonCistopher.fritz.box>
2024-04-23 21:57:20 +0200

    refactor: linter rules

--- lib/features/product/arbeitstische/presentation/widget/working_table_product_components.dart
+++ lib/features/product/arbeitstische/presentation/widget/working_table_product_components.dart
@@ -3,7 +3,6 @@ library my_product_widget;
 import 'package:app_flutter_produkt_bestellen/core/fix_values/app_colors.dart';
 import 'package:app_flutter_produkt_bestellen/core/fix_widgets/loading_widget.dart';
 import 'package:app_flutter_produkt_bestellen/features/product/arbeitstische/domain/entity/entity_product_workingtable.dart';
-import 'package:app_flutter_produkt_bestellen/features/product/arbeitstische/presentation/cubit/cubit_workingtable_product.dart';
 import 'package:app_flutter_produkt_bestellen/features/product/share/presentation/cubit/cubit_product.dart';
 import 'package:app_flutter_produkt_bestellen/features/product/share/presentation/cubit/state_product.dart';
 import 'package:app_flutter_produkt_bestellen/features/product/share/presentation/widget/widget_order_product.dart';
