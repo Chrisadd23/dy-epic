0c37205322d6cb79acd99ba53a2fd43e5e8607b2
Christopher Haas <christopherhaas@MBPvonCistopher.fritz.box>
2024-04-25 16:09:58 +0200

    fix:
      - get test from refactor/AOPB-9

---
+++ test/mock_data.dart
@@ -0,0 +1,5 @@
+//MockData wich is use for Unit-Tests
+import 'package:app_flutter_produkt_bestellen/features/product/arbeitstische/domain/entity/entity_product_workingtable.dart';
+
+const mockEntityWorkingTableProduct =
+    EntityWorkingTableProduct(name: '', productNumber: '', attributes: []);
