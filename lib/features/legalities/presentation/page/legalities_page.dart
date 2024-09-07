13358895f4d2affcc828d4914a849a9825e5d23f
flutter_developer <74181009+Chrisadd23@users.noreply.github.com>
2024-09-07 23:47:51 +0200

    feat: (#29)
    - change text size
    
    Co-authored-by: Christopher Haas <christopherhaas@MBPvonCistopher.fritz.box>

--- lib/features/legalities/presentation/page/legalities_page.dart
+++ lib/features/legalities/presentation/page/legalities_page.dart
@@ -1,3 +1,4 @@
+import 'package:app_flutter_produkt_bestellen/core/fix_values/app_text_style.dart';
 import 'package:app_flutter_produkt_bestellen/core/global_widgets/page/globa_scaffold.dart';
 import 'package:flutter/material.dart';
 
@@ -19,14 +20,18 @@ class _AppRights extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
+    return Center(
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Icon(Icons.copyright),
+          const Icon(
+            Icons.copyright,
+            size: 30,
+          ),
           Text(
             'Christopher Michael Haas',
             textAlign: TextAlign.center,
+            style: AppTextStyle.bold22,
           ),
         ],
       ),
